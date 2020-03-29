84a8fcefbb0629f05dc1388b0dcf3f054c7e6c50
Rim4i4ok <rim4i4ok@gmail.com>
2020-03-29 12:11:40 +0300

    test component rendering

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -6,7 +6,11 @@ import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders without error", () => {});
+test("renders without error", () => {
+	const wrapper = shallow(<App />);
+	const appComponent = wrapper.find("[data-test='component-app']");
+	expect(appComponent.length).toBe(1);
+});
 
 test("renders increment button", () => {});
 
