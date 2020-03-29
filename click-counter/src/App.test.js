ebbecb183b911bd28e7686fbb6b62f7313ab973c
Rim4i4ok <rim4i4ok@gmail.com>
2020-03-29 12:03:28 +0300

    first tests

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {});
+
+test("renders increment button", () => {});
+
+test("renders counter display", () => {});
+
+test("counter starts at 0", () => {});
+
+test("clicking button increments counter display", () => {});
