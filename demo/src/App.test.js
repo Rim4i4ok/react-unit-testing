82290c840d7d65d1722a345ee7cb46e729ac9ff0
Rim4i4ok <rim4i4ok@gmail.com>
2020-03-29 11:44:57 +0300

    first

---
+++ demo/src/App.test.js
@@ -0,0 +1,10 @@
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import React from "react";
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without crashong", () => {
+	const wrapper = shallow(<App />);
+});
