365f4fe880a328388d5a022261740a96a9a69fd4
Rim4i4ok <rim4i4ok@gmail.com>
2020-04-02 16:57:14 +0300

    Set up redux

---
+++ jotto/src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function  suucessReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @return {boolean} - new success state.
+ */
+export default (state, action) => {
+	return null;
+};
