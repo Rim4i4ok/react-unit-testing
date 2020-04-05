823ad8be5cc18922bbdf37605dc0d530462336c1
Rim4i4ok <rim4i4ok@gmail.com>
2020-04-05 13:27:07 +0300

    first test

--- jotto-ts/src/App.test.tsx
+++ jotto-ts/src/App.test.tsx
@@ -1,9 +1,10 @@
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({adapter: new EnzymeAdapter()});
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+})
