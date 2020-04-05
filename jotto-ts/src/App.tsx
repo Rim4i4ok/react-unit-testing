867167f7628301d222196a87291901107e3a92cb
Rim4i4ok <rim4i4ok@gmail.com>
2020-04-05 14:22:12 +0300

    test component rendering

--- jotto-ts/src/App.tsx
+++ jotto-ts/src/App.tsx
@@ -3,9 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>      
-    </div>
+    <div data-test="component-app">Hello World!</div>
   );
 }
 
