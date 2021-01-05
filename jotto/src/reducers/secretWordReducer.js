baba43d2865c6b82d6fe7fd9b2b5b404bf4c779d
Rim4i4ok <rim4i4ok@gmail.com>
2021-01-05 11:11:54 +0300

    removing all projects

--- jotto/src/reducers/secretWordReducer.js
+++
@@ -1,9 +0,0 @@
-/**
- * @function secretWordReducer
- * @param {array} state - Array of guessed words.
- * @param {object} action - action to be reduced.
- * @returns {string} - New state (secret word payload from action).
- */
-export default (state = null, action) => {
-	return state;
-};
