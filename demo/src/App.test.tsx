573f03a9ca730c7bb904893737aadd1a28d02eda
rim4i4ok <rim4i4ok@gmail.com>
2021-01-07 14:34:42 +0300

    connecting enzyme

--- demo/src/App.test.tsx
+++ demo/src/App.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  throw new Error;
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without fail', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
